90163ca34ae9266b46b363456a76eb152e1ceb0a
simplecss <simplecss@yandex.ru>
2016-09-18 12:51:45 +0300

    wet tot prod

--- src/js/settings/settings-dnevnik.js
+++ src/js/settings/settings-dnevnik.js
@@ -1,9 +1,9 @@
 export const OAuthOptions = {
-	provider: 'doveDnevnik',
+	provider: 'derinatDnevnik',
 	authUrl: 'https://login.dnevnik.ru/oauth2',
 	grantUrl: 'https://api.dnevnik.ru/v1/authorizations',
-	scope: 'Avatar,FullName,Birthday,Age,Roles,Files,Sex',	
-	clientId: '7d0d92280bd34aa9a5afec1c749bf0e1',
+	scope: 'Avatar,FullName,Roles,Lessons,Relatives',	
+	clientId: '1a3b7aca792d470cae448f41451e2ee1',
 	redirectUrl: 'https://ad.dnevnik.ru/promo/oauth2',
 }
 
@@ -16,10 +16,9 @@ export const PromoOptions = {
 	server: 'https://dnevnik.ru',
 }
 
-export const ForumOptions = {	
+export const CommentsOptions = {	
 	pageSize: 15,
 	adminId: [
-		'1000005449055',
 		'1000005031742',
 	],
 	anonAvatar: 'https://static.dnevnik.ru/images/avatars/user/a.m.jpg',
--- src/js/settings/settings-local.js
+++ src/js/settings/settings-local.js
@@ -1,20 +1,20 @@
-export const OAuthOptions = {
-	provider: 'doveLocal',
-	authUrl: 'https://login.staging.dnevnik.ru/oauth2',
-	grantUrl: 'https://api.staging.dnevnik.ru/v1/authorizations',
-	scope: 'Avatar,FullName,Birthday,Age,Roles,Schools,Organizations,EduGroups,Lessons,Marks,EduWorks,Relatives,Files,Contacts,Friends,Groups,Networks,Events,Wall,Messages,EmailAddress,Sex,SocialEntityMembership',	
-	clientId: '5123975fe9eb415390fb7aa316a15e4e',
-	redirectUrl: '//localhost:9000/oauth.html',
-}
+// export const OAuthOptions = {
+// 	provider: 'derinatLocal',
+// 	authUrl: 'https://login.staging.dnevnik.ru/oauth2',
+// 	grantUrl: 'https://api.staging.dnevnik.ru/v1/authorizations',
+// 	scope: 'Avatar,FullName,Roles,Lessons,Relatives',	
+// 	clientId: '1a3b7aca792d470cae448f41451e2ee1',
+// 	redirectUrl: '//localhost:9000/oauth.html',
+// }
 
-export const APIoptions = {	
-	base: 'https://api.staging.dnevnik.ru/v1/',
-}
+// export const APIoptions = {	
+// 	base: 'https://api.staging.dnevnik.ru/v1/',
+// }
 
-export const PromoOptions = {	
-	url: 'http://localhost:9000',
-	server: 'https://staging.dnevnik.ru',
-}
+// export const PromoOptions = {	
+// 	url: 'http://localhost:9000',
+// 	server: 'https://staging.dnevnik.ru',
+// }
 
 export const CommentsOptions = {	
 	pageSize: 15,
@@ -23,4 +23,24 @@ export const CommentsOptions = {
 		'1000001035607'
 	],
 	anonAvatar: 'https://static.dnevnik.ru/images/avatars/user/a.m.jpg',
+}
+
+
+export const OAuthOptions = {
+	provider: 'derinatStaging',
+	authUrl: 'https://login.dnevnik.ru/oauth2',
+	grantUrl: 'https://api.dnevnik.ru/v1/authorizations',
+	scope: 'Avatar,FullName,Roles,Lessons,Relatives',	
+	clientId: '1a3b7aca792d470cae448f41451e2ee1',
+	redirectUrl: '//localhost:9000/oauth.html',
+}
+
+export const APIoptions = {	
+	base: 'https://api.dnevnik.ru/v1/',
+}
+
+export const PromoOptions = {	
+	url: 'http://localhost:9000',
+	server: 'https://staging.dnevnik.ru',
+	cdn: 'http://localhost:9000/assets/',
 }
